371b32617a8182a8d71a60c412a97c8609d122f4
wangrun <yeesunday@gmail.com>
2017-03-22 22:12:17 +0800

    remove gitbook link

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp"),
   deploy = require("gulp-gh-pages");
 
 gulp.task('deploy', function () {
-  gulp.src("_book/**/*.*")
+  return gulp.src("_book/**/*.*")
     .pipe(deploy())
     .on("error", function(err){
       console.log(err)
